8fa017f89c404de94311a9ea9765c01ea420fc0d
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-03-27 18:26:48 +0700

    fix menu drawer handler

--- src/components/MenuDrawer.jsx
+++ src/components/MenuDrawer.jsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import { useState } from "react";
-import Link from 'next/link'
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,32 +16,42 @@ import {
 } from "@/components/ui/sheet";
 
 export function MenuDrawer({ className }) {
-    const [open, setOpen] = useState(false);
-    return (
-      <div className={className}>
-        <Sheet open={open} onOpenChange={setOpen}>
-          {open ? (
-            <button
-              className="text-xl font-bold"
-              onClick={() => setOpen(false)}
-            >
-              CLOSE
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={className}>
+      <Sheet open={open} onOpenChange={setOpen}>
+        {open ? (
+          <button
+            className="text-xl font-bold"
+            onClick={() => setOpen(false)}
+          >
+            CLOSE
+          </button>
+        ) : (
+          <SheetTrigger asChild>
+            <button className="text-xl font-bold">
+              MENU
             </button>
-          ) : (
-            <SheetTrigger asChild>
-              <button className="text-xl font-bold">
-                MENU
-              </button>
-            </SheetTrigger>
-          )}
-          <div>
-            <SheetContent className="md:max-w-none md:w-1/2 max-w-sm w-3/4 flex flex-col gap-72">
-              <div className="flex justify-between w-full mt-8">
-                <div>
-                  <div className="font-semibold text-sm flex flex-col gap-2 ">
-                    <Link href="/catalog">ALL</Link>
-                    <Link href="/catalog/batch-1">BATCH 1</Link>
-                    {/* <Link href="/catalog">LACOSTE</Link>
+          </SheetTrigger>
+        )}
+        <div>
+          <SheetContent className="md:max-w-none md:w-1/2 max-w-sm w-3/4 flex flex-col gap-72">
+            <div className="flex justify-between w-full mt-8">
+              <div>
+                <div className="font-semibold text-sm flex flex-col gap-2 ">
+                  <Link
+                    href="/catalog"
+                    onClick={closeDrawer}
+                  >
+                    ALL
+                  </Link>
+                  <Link
+                    href="/catalog/batch-1"
+                    onClick={closeDrawer}
+                  >
+                    BATCH 1
+                  </Link>
+                  {/* <Link href="/catalog">LACOSTE</Link>
                     <Link href="/catalog">TOPS</Link>
                     <Link href="/catalog">BOTTOM</Link>
                     <Link href="/catalog">FRAGRANCE</Link>
@@ -49,35 +59,33 @@ export function MenuDrawer({ className }) {
                     <Link href="/catalog">ACCESSORIES</Link>
                     <Link href="/catalog">LUGGAGE</Link>
                     <Link href="/catalog">FOOTWEAR</Link> */}
-                  </div>
-                </div>
-                <div>
-                  <div className="text-sm flex flex-col gap-2 text-end [&>*]:cursor-pointer">
-                    <Link href="/catalog">catalog</Link>
-                    <Link href="/catalog">broadcast</Link>
-                    <Link href="/catalog">instagram</Link>
-                    <Link href="/catalog">store</Link>
-                    <Link href="/catalog">privacy</Link>
-                    <Link href="/catalog">accessibiaty</Link>
-                    <Link href="/catalog">terms</Link>
-                  </div>
                 </div>
               </div>
-              <SheetFooter>
-                <div className="flex flex-col justify-center items-center gap-8">
-                  <Button
-                    className="text-black font-semibold rounded-full"
-                  >
-                    SUBSCRIBE TO OUR NEWSLETTER
-                  </Button>
-                  <p className="font-semibold">
-                    Martinee Studio
-                  </p>
+              <div>
+                <div className="text-sm flex flex-col gap-2 text-end [&>*]:cursor-pointer">
+                  <Link href="/catalog">catalog</Link>
+                  <Link href="/catalog">broadcast</Link>
+                  <Link href="/catalog">instagram</Link>
+                  <Link href="/catalog">store</Link>
+                  <Link href="/catalog">privacy</Link>
+                  <Link href="/catalog">accessibiaty</Link>
+                  <Link href="/catalog">terms</Link>
                 </div>
-              </SheetFooter>
-            </SheetContent>
-          </div>
-        </Sheet>
-      </div>
-    );
-  }
+              </div>
+            </div>
+            <SheetFooter>
+              <div className="flex flex-col justify-center items-center gap-8">
+                <Button className="text-black font-semibold rounded-full">
+                  SUBSCRIBE TO OUR NEWSLETTER
+                </Button>
+                <p className="font-semibold">
+                  Martinee Studio
+                </p>
+              </div>
+            </SheetFooter>
+          </SheetContent>
+        </div>
+      </Sheet>
+    </div>
+  );
+}
